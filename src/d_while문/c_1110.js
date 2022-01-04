c1ccae46b6e6cb25b9ebe1fe8a9448cb565fb7f5
reload1bronze <nature9th@gmail.com>
2022-01-04 21:36:37 +0900

    4단계 while문 (1110번: 더하기 사이클)

---
+++ src/d_while문/c_1110.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+let N = parseInt(fs.readFileSync("/dev/stdin").toString());
+let result = N;
+let count = 0;
+
+do {
+  let sum = (Math.floor(result / 10) + (result % 10)) % 10;
+  result = (result % 10) * 10 + sum;
+  count++;
+} while (result !== N);
+
+console.log(count);
