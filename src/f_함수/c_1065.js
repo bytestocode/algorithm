c62243b719abb06ad5e3ce1a6b26d7518994d686
reload1bronze <nature9th@gmail.com>
2022-01-13 20:55:52 +0900

    6단계 함수 (1065번: 한수)

---
+++ src/f_함수/c_1065.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const N = +fs.readFileSync("/dev/stdin").toString().trim();
+
+function isAnswer(num) {
+  const numStr = `${num}`;
+  return +num < 100 || +numStr[0] - +numStr[1] === +numStr[1] - +numStr[2];
+}
+
+let count = 0;
+
+for (let i = 1; i < N + 1; i++) {
+  if (isAnswer(i)) {
+    count += 1;
+  }
+}
+
+console.log(count);
