28b47d425a69abb3436eafe60f8a0d04bd21f7ff
reload1bronze <nature9th@gmail.com>
2022-04-13 10:33:20 +0900

    구현 (해시 테이블) -

---
+++ src/구현/1_선형 자료구조/hashTable.js
@@ -0,0 +1,86 @@
+class HashTable {
+  constructor() {
+    this.table = new Array(127);
+    this.size = 0;
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % this.table.length;
+  }
+
+  set(key, value) {
+    const index = this._hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i][0] === key) {
+          this.table[index][i][1] = value;
+          return;
+        }
+      }
+      this.table[index].push([key, value]);
+    } else {
+      this.table[index] = [];
+      this.table[index].push([key, value]);
+    }
+    this.size += 1;
+  }
+
+  get(key) {
+    const index = this._hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i][0] === key) {
+          return this.table[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    const index = this._hash(key);
+
+    if (this.table[index] && this.table[index].length) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i][0] === key) {
+          this.table[index].splice(i, 1);
+          this.size -= 1;
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  logAll() {
+    this.table.forEach((values, index) => {
+      const chainedValues = values.map(
+        ([key, value]) => `[ ${key}: ${value} ]`
+      );
+      console.log(`${index}: ${chainedValues}`);
+    });
+  }
+}
+
+const ht = new HashTable();
+
+ht.set("Korea", 777);
+ht.set("China", 888);
+ht.set("Japan", 111);
+
+ht.logAll();
+// 102: [ China: 888 ]
+// 109: [ Japan: 111 ]
+// 117: [ Korea: 777 ]
+
+console.log(ht.size); // 3
+console.log(ht.get("Korea")); // 777
+ht.remove("Japan");
+ht.logAll();
+// 102: [ China: 888 ]
+// 109:
+// 117: [ Korea: 777 ]
